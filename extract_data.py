b6813510972d17edd59b7bd516f2e24d19a5fdfa
Jerrold <jerroldfisher@live.com>
2024-03-28 18:59:24 +0800

    extract_data

--- extract_data.py
+++ extract_data.py
@@ -1,17 +1,33 @@
 import csv
 import boto3
-import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder\
+    .appName("Extract from S3")\
+    .getOrCreate()
 
 with open('Jerrold_accessKeys.csv') as file:
     reader = csv.reader(file, delimiter=',')
     next(reader)
     access_key_id, secret_access_key = next(reader)
 
-bucket = 'dvd-rental-data'
 
 s3 = boto3.client('s3',
                   aws_access_key_id=access_key_id,
                   aws_secret_access_key=secret_access_key)
 
-s3.download_file('dvd-rental-data', 'folder1/address.dat', 'input/address.dat')
+print('-----------------')
+# Set the S3 path
+s3_path = "s3a://dvd-rental-data/folder1/address.dat"
 
+# Set Hadoop configuration for S3 access
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.access.key", access_key_id)
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.secret.key", secret_access_key)
+
+# Read data into DataFrame
+df = spark.read.csv(s3_path)#,
+                    #sep='\t',
+                    #schema='address_id int, address string, address2 string, district string, city_id int, postal_code int, phone string, last_update timestamp')
+
+df.printSchema()
+df.show()
